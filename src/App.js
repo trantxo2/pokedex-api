2b05b7d75c21758f51db21ff2aad74c46e527216
trantxo2 <trantxo2@gmail.com>
2023-05-13 12:21:40 +0200

    primer commit: añadida la primera tarjeta con titulo y numero

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="card">
+          <div className="cardInfo">
+            <h1 className="title">Bulbasaur</h1>
+            <h2 className="number">#001</h2>
+          </div>
+          <div className="cardData"></div>
+        </div>
       </header>
     </div>
   );
