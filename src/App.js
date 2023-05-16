a3469087d5ac7f619e915ec2ec510ba4e4fccb9f
trantxo2 <trantxo2@gmail.com>
2023-05-16 19:57:39 +0200

    iniciado componente card

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
 import "./index.css";
 import "./App.css";
+import { Card } from "./Card";
+
+const pokemon = {
+  id: "001",
+  name: "Bulbasaur",
+  height: 4.3,
+  weight: 6.5,
+  types: ["grass", "poison"],
+  src: "https://...",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet facilisis mi. Integer eget urna eu mauris scelerisque ornare. Nam at finibus purus.",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <body>
-        <div className="card">
-          <div className="cardInfo">
-            <h1 className="title">Bulbasaur</h1>
-            <h2 className="number">#001</h2>
-          </div>
-          <div className="cardData">
-            <img
-              className="pokemonImage"
-              src="images/bulbasaur.svg"
-              alt=""
-            ></img>
-            <div className="types">
-              <h3 className="type grassType">Grass</h3>
-              <h3 className="type poisonType">Poison</h3>
-            </div>
-            <div className="about">About</div>
-            <div className="specs">
-              <div className="specs__container">
-                <div className="specs__data">
-                  <img
-                    className="specs__image"
-                    src="images/Vector.svg"
-                    alt=""
-                  ></img>
-                  <h4>6,9 kg</h4>
-                </div>
-                <h5 className="specs__name">Weight</h5>
-              </div>
-              <div className="divider"></div>
-              <div className="specs__container">
-                <div className="specs__data">
-                  <img
-                    className="specs__image"
-                    src="images/Height.svg"
-                    alt=""
-                  ></img>
-                  <h4>0,7 m</h4>
-                </div>
-                <h5 className="specs__name">Height</h5>
-              </div>
-            </div>
-            <div className="description">
-              There is a plant seed on its back right from the day this Pokémon
-              is born.
-            </div>
-          </div>
-        </div>
+        <Card />
       </body>
     </div>
   );
---
+++ src/Card.jsx
@@ -0,0 +1,47 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="cardInfo">
+        <h1 className="title">Bulbasaur</h1>
+        <h2 className="number">#001</h2>
+      </div>
+      <div className="cardData">
+        <img className="pokemonImage" src="images/bulbasaur.svg" alt=""></img>
+        <div className="types">
+          <h3 className="type grassType">Grass</h3>
+          <h3 className="type poisonType">Poison</h3>
+        </div>
+        <div className="about">About</div>
+        <div className="specs">
+          <div className="specs__container">
+            <div className="specs__data">
+              <img
+                className="specs__image"
+                src="images/Vector.svg"
+                alt=""
+              ></img>
+              <h4>6,9 kg</h4>
+            </div>
+            <h5 className="specs__name">Weight</h5>
+          </div>
+          <div className="divider"></div>
+          <div className="specs__container">
+            <div className="specs__data">
+              <img
+                className="specs__image"
+                src="images/Height.svg"
+                alt=""
+              ></img>
+              <h4>0,7 m</h4>
+            </div>
+            <h5 className="specs__name">Height</h5>
+          </div>
+        </div>
+        <div className="description">
+          There is a plant seed on its back right from the day this Pokémon is
+          born.
+        </div>
+      </div>
+    </div>
+  );
+};
